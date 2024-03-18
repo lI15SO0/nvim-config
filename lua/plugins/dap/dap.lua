1a316b33e3727fe10ebea5d3d13c1b173570fd6f
lI15SO0 <svvivi@outlook.com>
2024-03-19 04:12:47 +0800

    add(plugins/dap): nvim-nio as a dependencies for "nvim-dap-ui"

--- lua/plugins/dap/dap.lua
+++ lua/plugins/dap/dap.lua
@@ -4,6 +4,7 @@ return {
 		"rcarriga/nvim-dap-ui",
 		"theHamsta/nvim-dap-virtual-text",
 		"nvim-telescope/telescope-dap.nvim",
+		"nvim-neotest/nvim-nio",
 	},
 	event = { "BufReadPost", "BufNewFile" },
 	config = function()
