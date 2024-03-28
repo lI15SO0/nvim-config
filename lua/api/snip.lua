b76adfc950fd814f22678ba40a0ea091d7b1089e
lI15SO0 <svvivi@outlook.com>
2024-03-29 00:23:42 +0800

    fix(api/snip): bring snip api's function back.

--- lua/api/snip.lua
+++ lua/api/snip.lua
@@ -3,10 +3,10 @@ local M = {}
 function M.add_snip_dir(path)
 	local options = require("core.options")
 	if type(path) == "string" then
-		table.insert(options.snip.snippath, path)
+		table.insert(options.snip.paths, path)
 	elseif type(path) == "table" then
 		for _, p in pairs(path) do
-			table.insert(options.snip.snippath, p)
+			table.insert(options.snip.paths, p)
 		end
 	end
 end
