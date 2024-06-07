e6cffdced2651737d4436cb8f27de7e6762975cd
lI15SO0 <svvivi@outlook.com>
2024-06-08 01:10:28 +0800

    feat(plugin/view/aerial): Bind key to aerial nav.

--- lua/plugins/view/aerial.lua
+++ lua/plugins/view/aerial.lua
@@ -1,13 +1,16 @@
 -- https://github.com/stevearc/aerial.nvim
+local options = require("core.options")
+
 return {
 	"stevearc/aerial.nvim",
 	keys = {
-		{ '<leader>a', '<cmd>AerialToggle!<cr>', mode = 'n', desc = "Toggle Aerial" },
-		{ '{',         '<cmd>AerialPrev<cr>',    mode = 'n', desc = "Move cursor to Perv navigation" },
-		{ '}',         '<cmd>AerialNext<cr>',    mode = 'n', desc = "Move cursor to Next navigation" },
+		{ '<leader>a', '<cmd>AerialToggle!<cr>',   mode = 'n', desc = "Toggle Aerial" },
+		{ '<leader>s', '<cmd>AerialNavToggle<cr>', mode = 'n', desc = "Toggle Aerial" },
+		{ '{',         '<cmd>AerialPrev<cr>',      mode = 'n', desc = "Move cursor to Perv navigation" },
+		{ '}',         '<cmd>AerialNext<cr>',      mode = 'n', desc = "Move cursor to Next navigation" },
 	},
 	config = function()
-		require("aerial").setup({
+		require("aerial").setup {
 			layout = {
 				min_width = 30,
 			},
@@ -15,6 +18,39 @@ return {
 			filter_kind = false,
 			nerd_font = "auto",
 			update_events = "TextChanged,InsertLeave",
-		})
+
+			show_guides = true,
+
+			nav = {
+				border = "rounded",
+				max_height = 0.9,
+				min_height = { 10, 0.7 },
+				max_width = 0.3,
+				min_width = { 0.3, 20 },
+
+				win_opts = {
+					cursorline = true,
+					winblend = 10,
+				},
+				-- Jump to symbol in source window when the cursor moves
+				autojump = false,
+				-- Show a preview of the code in the right column, when there are no child symbols
+				preview = true,
+				-- Keymaps in the nav window
+				keymaps = {
+					["<CR>"] = "actions.jump",
+					["o"] = "actions.jump",
+					["<2-LeftMouse>"] = "actions.jump",
+					["<C-v>"] = "actions.jump_vsplit",
+					["<C-s>"] = "actions.jump_split",
+					["h"] = "actions.left",
+					["l"] = "actions.right",
+					["<C-c>"] = "actions.close",
+					["q"] = "actions.close",
+					["<esc>"] = "actions.close",
+				}
+			}
+
+		}
 	end
 }
