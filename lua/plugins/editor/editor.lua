e1f7514c070fadf581a24d1c62d4280f73851aa9
lI15SO0 <svvivi@outlook.com>
2025-01-09 01:27:51 +0800

    remove(plugins/glow.nvim): I was almost forget this plugin.

--- lua/plugins/editor/editor.lua
+++ lua/plugins/editor/editor.lua
@@ -1,11 +1,5 @@
 return {
 	{
-		"ellisonleao/glow.nvim",
-		lazy = true,
-		ft = {"md", "markdown"},
-		config = true
-	},
-	{
 		"ur4ltz/surround.nvim",
 		event = {"BufReadPost", "BufAdd"},
 		config = true
