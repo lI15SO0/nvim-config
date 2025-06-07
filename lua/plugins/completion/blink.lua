8f394d643e7178e3f60ce79d915f2ddae0b3824e
lI15SO0 <svvivi@outlook.com>
2025-06-08 02:27:53 +0800

    new_plugin: add neodev to help build nvim lua.

--- lua/plugins/completion/blink.lua
+++ lua/plugins/completion/blink.lua
@@ -52,7 +52,12 @@ return
 				providers = {
 					lsp = {
 						async = true,
-					}
+					},
+					lazydev = {
+						name = "LazyDev",
+						module = "lazydev.integrations.blink",
+						score_offset = 100,
+					},
 				},
 			},
 			fuzzy = { implementation = 'prefer_rust_with_warning' },
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -3,6 +3,7 @@ local api = require("api")
 local path="plugins.lsp"
 local plugins = {
 	"lspui",
+	"lazydev"
 }
 
 return api.plugin.construct(path, plugins)
