5535ad2f1e5d8b4c5b5859eeb0c989ae18426035
lI15SO0 <svvivi@outlook.com>
2026-01-13 09:44:24 +0800

    fix(plug/treesitter/treesitter): make it use "master" branch to ensurce compatibility.

--- lua/plugins/treesitter/treesitter.lua
+++ lua/plugins/treesitter/treesitter.lua
@@ -6,6 +6,7 @@ return {
 			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
 		event = { "BufRead", "BufAdd" },
+		branch = "master",
 
 		main = "nvim-treesitter.configs",
 		build = ":TSUpdate",
