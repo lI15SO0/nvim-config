aa2cf70211554406cfa26757185e0abd18fe3477
lI15SO0 <svvivi@outlook.com>
2026-01-16 04:08:19 +0800

    feat(setting/clipboard): enable osc52 as clipboard method.

--- lua/core/setting.lua
+++ lua/core/setting.lua
@@ -4,6 +4,17 @@ local settings = {}
 
 settings.g = {
 	mapleader = "`",
+	clipboard = {
+		name = "OSC 52",
+		copy = {
+			['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+			['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+		},
+		paste = {
+			['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+			['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+		}
+	}
 }
 
 settings.o = {
