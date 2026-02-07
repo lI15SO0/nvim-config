d7a462842cde4bf50f3925b39bc84648f4cb69cd
lI15SO0 <svvivi@outlook.com>
2026-02-08 01:14:13 +0800

    fix(lsp/neocmakelsp): fix wrong startup arg.

--- lsp/neocmake.lua
+++ lsp/neocmake.lua
@@ -3,7 +3,7 @@
 
 ---@type vim.lsp.Config
 return {
-  cmd = { 'neocmakelsp', '--stdio' },
+  cmd = { 'neocmakelsp', 'stdio' },
   filetypes = { 'cmake' },
   root_markers = { '.git', 'build', 'cmake', 'CmakeLists.txt'},
 }
