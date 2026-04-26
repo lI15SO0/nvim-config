5f9e38035df92fb616e5d7780f15d25ca79fd7fe
lI15SO0 <svvivi@outlook.com>
2026-04-27 05:44:48 +0800

    fix(api.env): fix wrong comment for this function  && perf(api.env): make function robuster.

--- lua/api/env.lua
+++ lua/api/env.lua
@@ -1,14 +1,13 @@
 local M = {}
 
 --- select a valid cmd from cmds.
---- @param cmds table<string>
---- @return table|nil 
+--- @param cmds table<table<string>>
+--- @return table|nil
 function M.cmd_select(cmds)
+	if not cmds then return nil end
 	for _, v in ipairs(cmds) do
-		if #v ~= 0 then
-			if vim.fn.executable(v[1]) == 1 then
-				return v
-			end
+		if v and v[1] and vim.fn.executable(v[1]) == 1 then
+			return v
 		end
 	end
 	return nil
