94d570498430098ef17fcc08ab0d76ff258a18b4
lI15SO0 <svvivi@outlook.com>
2026-06-19 17:40:39 +0800

    replace(editor/surround): use kylechui/nvim-surround replace old surround plugin.

--- lua/plugins/editor/surround.lua
+++ lua/plugins/editor/surround.lua
@@ -2,15 +2,72 @@ local api = require("api")
 local gh = api.plugin.gh
 
 vim.pack.add({
-	{ src = gh("ur4ltz/surround.nvim") }
+	{ src = gh("kylechui/nvim-surround") }
 })
 
 vim.api.nvim_create_autocmd({ "BufReadPost", "BufAdd" }, {
 	group = vim.api.nvim_create_augroup("LoadSurround", { clear = true }),
 	once = true,
 	callback = function()
-		require("surround").setup({
-			space_on_closing_char = true,
+		vim.g.nvim_surround_no_normal_mappings = true
+		vim.g.nvim_surround_no_visual_mappings = true
+
+		local surround = require("nvim-surround")
+
+		surround.setup {
+			surrounds = {
+				["("] = { add = { "(", ")" } },
+				[")"] = { add = { "(", ")" } },
+				["["] = { add = { "[", "]" } },
+				["]"] = { add = { "[", "]" } },
+				["{"] = { add = { "{", "}" } },
+				["}"] = { add = { "{", "}" } },
+				["<"] = { add = { "<", ">" } },
+				[">"] = { add = { "<", ">" } },
+				["q"] = { add = { "'", "'" } },
+				["Q"] = { add = { '"', '"' } },
+				["b"] = { add = {"(", ")"} },
+				["B"] = { add = {"{", "}"} },
+				["l"] = { add = {"[", "]"} },
+				["t"] = { add = {"<", ">"} },
+			},
+			aliases = {
+				q = { '"', "'", '`' },
+				Q = { '"' },
+				b = { '(', '[', '{' },
+				B = { '{' },
+			}
+		}
+
+		api.map.bulk_regist({
+			{
+				mode = { 'n' },
+				key = 'sa',
+				fn = "<Plug>(nvim-surround-normal)",
+				options = { silent = true },
+				description = 'use Telescope to find files',
+			},
+			{
+				mode = { 'x' },
+				key = 's',
+				fn = "<Plug>(nvim-surround-visual)",
+				options = { silent = true },
+				description = 'use Telescope to find files',
+			},
+			{
+				mode = { 'n' },
+				key = 'sd',
+				fn = "<Plug>(nvim-surround-delete)",
+				options = { silent = true },
+				description = 'use Telescope to find files',
+			},
+			{
+				mode = { 'n' },
+				key = 'sr',
+				fn = "<Plug>(nvim-surround-change)",
+				options = { silent = true },
+				description = 'use Telescope to find files',
+			},
 		})
 	end,
 })
