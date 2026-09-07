5f7937f7743fb4d03857d193a9fde24c7f351480
lI15SO0 <svvivi@outlook.com>
2026-09-08 03:45:00 +0800

    fix(core/lsp/attach): now it will set foldmethod when lsp atttach.

--- lua/core/lsp/attach.lua
+++ lua/core/lsp/attach.lua
@@ -16,6 +16,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		-- Folding
 		if client and client:supports_method 'textDocument/foldingRange' then
 			local win = vim.api.nvim_get_current_win()
+			vim.wo[win][0].foldmethod = 'expr'
 			vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
 		end
 
